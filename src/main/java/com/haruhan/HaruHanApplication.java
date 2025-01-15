04590349cc43af0709a902b575107ad7f8cad86f
jjindol <andy15948@naver.com>
2025-01-15 23:22:34 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/haruhan/HaruHanApplication.java
@@ -0,0 +1,13 @@
+package com.haruhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruHanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruHanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haruhan/HaruHanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruHanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
