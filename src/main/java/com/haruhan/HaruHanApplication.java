88e6d8949eb16664285bc752c266b449baa6aa53
DongHeon Kwak <rhkrehdgjs@naver.com>
2025-01-23 16:43:15 +0900

    merge : main merge test

--- src/main/java/com/haruhan/HaruHanApplication.java
+++ src/main/java/com/haruhan/HaruHanApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HaruHanApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HaruHanApplication.class, args);
     }
